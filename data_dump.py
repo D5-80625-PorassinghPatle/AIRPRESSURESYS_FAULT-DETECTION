a82a43402dc88ee2d20455fcc81254c906a2b932
PORASSINGHPATLE <poraspatle@gmail.com>
2023-01-29 13:01:43 +0530

    this is our version 1.00001

---
+++ data_dump.py
@@ -0,0 +1,43 @@
+import pymongo
+import pandas as pd
+import json
+
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME="airpressuresys"
+COLLECTION_NAME="sensor"
+
+if __name__=="__main__":
+
+    df=pd.read_csv(DATA_FILE_PATH)
+
+    print(F"ROWS AND COLUMN: {df.shape}")
+
+
+    ##convert dataframe in jason format so that we can dump record in mongodb
+
+    df.reset_index(drop=True,inplace=True )
+
+    json_record=list(json.loads(df.T.to_json()).values())
+
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+    ## git --version 
+    ## git add
+    ## git remote v
+    ## git remote --v
+    ## git reset 
+
+    
+
+
+
+
+
+
+
+
+
