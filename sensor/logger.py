455c48753ba6e0241b67e3e2ba69595643f06be6
PORASSINGHPATLE <poraspatle@gmail.com>
2023-01-30 13:24:31 +0530

    logger configured i.e created logging module

---
+++ sensor/logger.py
@@ -0,0 +1,33 @@
+import logging
+import os
+from datetime import datetime
+
+#log file name 
+
+LOG_FILE_NAME=f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+
+#Create log directory
+
+LOG_FILE_DIR=os.path.join(os.getcwd(),"logs")
+
+#create directory if not availabel 
+
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
+
+# log file path
+
+LOG_FILE_PATH=os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+logging.basicConfig(filename=LOG_FILE_NAME,
+    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+   level=logging.INFO,
+)
+
+
+
+
+
+
+
+
+
